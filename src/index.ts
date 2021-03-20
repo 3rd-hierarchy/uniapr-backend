35de1a1d7f3e0792044306f35871497c83b07682
Senioris <3rd.hierarchy@gmail.com>
2021-03-20 23:38:19 +0900

    Develop (#4)
    * changes index.html
    
    * add dist files
    
    * make client commons file
    
    * add sushiswap
    
    * change sushiswap name
    
    * update client files
    
    * update client files
    
    * update client files
    
    * update client
    
    * update client
    
    * add batch schedule
    
    * build production files
    
    * add cluster
    
    Co-authored-by: Senioris <crypto.seln@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { config } from './config/config'
 import { app } from './express'
 import * as mongoose from 'mongoose'
 import { Logger } from './utils/logger'
+import * as cluster from 'cluster'
 
 mongoose.connect(config['mongoUri'], {
   useNewUrlParser: true,
@@ -15,6 +16,19 @@ mongoose.connection.on('error', () => {
   throw new Error(`unable to connect to database: ${config['mongoUri']}`)
 })
 
-app.listen(config['port'], () => {
-  Logger.instance.logger?.info('Server started on port %s.', config['port'])
+var numCPUs = require('os').cpus().length;
+if (cluster.isMaster) {
+  for (var i = 0; i < numCPUs; i++) {
+    // Create a worker
+    cluster.fork();
+  }
+} else {
+  app.listen(config['port'], () => {
+    Logger.instance.logger?.info('Server started on port %s.', config['port'])
+  })
+}
+
+cluster.on('exit', function (worker, code, signal) {
+  console.log('Worker %d died with code/signal %s. Restarting worker...', worker.process.pid, signal || code);
+  cluster.fork();
 })
