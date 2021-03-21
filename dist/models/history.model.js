ae43bc46016fd8d288661ef622fce851d5893ea0
Senioris <3rd.hierarchy@gmail.com>
2021-03-21 13:44:12 +0900

    Develop (#12)
    * fix security error
    
    * commit miss
    
    * try fix security error
    
    * test
    
    * test
    
    * test
    
    * change security policy in express
    
    * update dist file
    
    * refactor public path structures.
    
    * delete dist directory
    
    * exclude dist on git
    
    Co-authored-by: Senioris <crypto.seln@gmail.com>

--- dist/models/history.model.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.HistoryModel = void 0;
-const mongoose_1 = require("mongoose");
-const history_schema_1 = require("./history.schema");
-exports.HistoryModel = mongoose_1.model("History", history_schema_1.HistorySchema);
