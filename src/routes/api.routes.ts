f1400d8dae0e45040ec6ca925f2bc9c74b38932f
Senioris <crypto.seln@gmail.com>
2021-03-25 02:37:21 +0900

    add history api

--- src/routes/api.routes.ts
+++ src/routes/api.routes.ts
@@ -4,4 +4,5 @@ import * as historyController from '../controllers/history.controller'
 
 export const router = express.Router()
 
-router.route('/api/history').get(historyController.list)
+router.route('/api/list').get(historyController.list)
+router.route('/api/history').get(historyController.history)
